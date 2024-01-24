61a824dd1079f13ffe9eb56360843197ff02c384
maany <imptodefeat@gmail.com>
2024-01-24 21:57:25 +0100

    devops: component tests, CI, storybooks

--- tests/component/vitest.config.ts
+++ tests/component/vitest.config.ts
@@ -1,19 +1,19 @@
 import { defineProject, mergeConfig } from 'vitest/config'
-import path from 'path'
 import react from '@vitejs/plugin-react'
 import sharedConfig from '../../vitest.shared'
 
-export default mergeConfig(
+const config =  mergeConfig(
   sharedConfig,
   defineProject({
     test: {
+      name: 'component',
       environment: 'jsdom',
+      include: ['**/*.test.tsx', '**/*.test.ts'],
     },
-    resolve: {
-      alias: {
-        "~": path.resolve(__dirname, "../", "../", "src"),
-      }
-    },
+   
     plugins: [react()]
   })
 )
+
+console.log(config)
+export default config

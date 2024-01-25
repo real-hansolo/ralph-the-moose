a372ec10443ed7ea9d166bdb72b63ff23fd82163
maany <imptodefeat@gmail.com>
2024-01-25 02:17:53 +0100

    setup highlight ( only in production mode)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,20 @@
 import "~/styles/globals.css";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { AxiomWebVitals } from "next-axiom";
+import { HighlightInit } from '@highlight-run/next/client'
 
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
-import { AxiomWebVitals } from "next-axiom";
+
+import { env } from "~/env";
+
+// Set up Highlight ONLY in production
+const NODE_ENV = env.NODE_ENV ?? 'no-env'
+let HIGHLIGHT_PROJECT_ID = 'no-project-id'
+if (NODE_ENV === 'production') {
+  HIGHLIGHT_PROJECT_ID = env.HIGHLIGHT_PROJECT_ID ?? 'no-project-id'
+}
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,13 +32,26 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  console.log('HIGHLIGHT_PROJECT_ID', HIGHLIGHT_PROJECT_ID)
   return (
-    <html lang="en">
-      <AxiomWebVitals />
-      <SpeedInsights />
-      <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
-      </body>
-    </html>
-  );
+    <>
+      <HighlightInit
+				projectId={HIGHLIGHT_PROJECT_ID}
+				serviceName="ralph-the-moose-frontend"
+				tracingOrigins
+				networkRecording={{
+					enabled: true,
+					recordHeadersAndBody: true,
+					urlBlocklist: [],
+				}}
+			/>
+      <html lang="en">
+        <AxiomWebVitals />
+        <SpeedInsights />
+        <body className={`font-sans ${inter.variable}`}>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </body>
+      </html>
+    </>
+  )
 }

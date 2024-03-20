ddbae16cd6944ea1475b202730c54cb778ccc1ea
maany <imptodefeat@gmail.com>
2024-03-20 23:55:32 +0100

    add more wallets

--- src/lib/infrastructure/config/wallets.ts
+++ src/lib/infrastructure/config/wallets.ts
@@ -1,6 +1,12 @@
-import { coinbaseWallet, metamaskWallet } from "@thirdweb-dev/react";
+import { bloctoWallet, coinbaseWallet, frameWallet, localWallet, metamaskWallet, phantomWallet, trustWallet, walletConnect } from "@thirdweb-dev/react";
 
 export const SUPPORTED_WALLETS = [
     metamaskWallet(),
+    walletConnect({ recommended: true }),
     coinbaseWallet(),
+    localWallet(),
+    trustWallet({ recommended: true }),
+    bloctoWallet(),
+    frameWallet(),
+    phantomWallet(),
 ];

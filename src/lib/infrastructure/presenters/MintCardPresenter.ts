04a203297d77e9cb8f16d43f972cafc707019959
maany <imptodefeat@gmail.com>
2024-03-21 06:18:20 +0100

    mint card <> indexer

--- src/lib/infrastructure/presenters/MintCardPresenter.ts
+++ src/lib/infrastructure/presenters/MintCardPresenter.ts
@@ -1,11 +1,15 @@
 import { type Signal, signal } from "@preact/signals-react";
 import type MintCardViewModel from "../view-models/MintCardViewModel";
+import IndexerGateway from "../gateways/indexer";
+import { env } from "~/env";
+import { GetAllocationLimitDTO, type GetAllMintedDTO } from "../dto/indexer-dto";
 
 export default class MintCardPresenter {
     public mintedPercentage: Signal<number>;
     public mintLimit: Signal<number>;
     public totalSupply: Signal<number>;
     public totalMinted: Signal<number>;
+    private indexer: IndexerGateway;
     constructor(
         // inject necessary gateways
     ) {
@@ -13,11 +17,36 @@ export default class MintCardPresenter {
         this.mintLimit = signal(0);
         this.mintedPercentage = signal(0);
         this.totalSupply = signal(0);
-        this.totalMinted = signal(0);
+        this.totalMinted = signal<number>(0);
+        const indexerHost = env.NEXT_PUBLIC_INDEXER_URL;
+        this.indexer = new IndexerGateway(indexerHost);
     }
 
     async present(): Promise<MintCardViewModel> {
         // present the view model
+        const allMinted: GetAllMintedDTO = await this.indexer.getAllMinted();
+        if(!allMinted.success) {
+            return {
+                status: "error",
+                message: allMinted.msg
+            } as MintCardViewModel;
+        } else if(allMinted.success) {
+            this.totalMinted.value = allMinted.data.total_minted / 1000000000; // TODO: check if this is the correct value
+        }
+
+        this.totalSupply.value = 5000000
+        
+        const allocationLimits: GetAllocationLimitDTO = await this.indexer.getAllocationLimits();
+        if(!allocationLimits.success) {
+            return {
+                status: "error",
+                message: allocationLimits.msg
+            } as MintCardViewModel;
+        } else if(allocationLimits.success) {
+            this.mintLimit.value = allocationLimits.data.total_mintable / 1000000000; // TODO: check if this is the correct value
+        }
+
+        this.mintedPercentage.value = parseFloat(((this.totalMinted.value / this.mintLimit.value) * 100).toFixed(2));
         return {
             status: "success",
             data: {

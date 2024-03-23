2e05aeb0309464f86e9a15303258b7d13c5c2929
maany <imptodefeat@gmail.com>
2024-03-23 23:54:33 +0100

    configure chains in thirdweb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 "use client";
 import { unstable_noStore as noStore } from "next/cache";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Base, BaseSepoliaTestnet } from "@thirdweb-dev/chains";
 import { useSignals } from "@preact/signals-react/runtime";
 import { SUPPORTED_WALLETS } from "~/lib/infrastructure/config/wallets";
-// import {
-//   DEFAULT_CHAIN,
-//   SUPPORTED_CHAINS,
-// } from "~/lib/infrastructure/config/chains";
+import {
+} from "~/lib/infrastructure/config/chains";
 import { RalphHome } from "./pages/RalphHome";
 import { env } from "~/env";
 
 export default function Home() {
   noStore();
   useSignals();
-
-  // const supportedChains  = Object.values(SUPPORTED_CHAINS).map((chain) => chain.thirdWeb);
+  const  defaultChain = env.NEXT_PUBLIC_ENABLE_TESTNETS ? BaseSepoliaTestnet : Base;
   return (
     <ThirdwebProvider
       supportedWallets={SUPPORTED_WALLETS}
+      supportedChains={[Base, BaseSepoliaTestnet]}
       clientId={env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
+      activeChain={defaultChain}
     >
       <RalphHome />
     </ThirdwebProvider>

e421ce71409a4a95aafff3292c30d09df1200488
maany <imptodefeat@gmail.com>
2024-03-23 03:52:24 +0100

    set inscription balance to 0 if wallet chain ! = active chain

--- src/lib/infrastructure/presenters/BalanceCardPresenter.ts
+++ src/lib/infrastructure/presenters/BalanceCardPresenter.ts
@@ -1,6 +1,7 @@
-import { signal, type Signal } from "@preact/signals-react";
+import { Signal, signal } from "@preact/signals-react";
 import type BalanceCardViewModel from "../view-models/BalanceCardViewModel";
 import type IndexerGateway from "../gateways/indexer";
+import { DEFAULT_CHAIN, type TChainConfig } from "../config/chains";
 
 export default class BalanceCardPresenter {
   public inscriptionBalance: Signal<number>;
@@ -8,22 +9,28 @@ export default class BalanceCardPresenter {
   public fee: Signal<number>;
   private indexer: IndexerGateway;
   private walletAddress: string | undefined;
-  constructor(indexer: IndexerGateway, walletAddress: string | undefined) {
+  private walletChainId: number | undefined;
+  private activeChain = new Signal<TChainConfig>(DEFAULT_CHAIN);
+  constructor(indexer: IndexerGateway, walletAddress: string | undefined, walletChainID: number | undefined) {
     this.inscriptionBalance = signal(0);
     this.wrappedBalance = signal(0);
     this.fee = signal(0);
     this.indexer = indexer;
     this.walletAddress = walletAddress;
+    this.walletChainId = walletChainID;
   }
 
   async __presentInscriptionBalance() {
-    const latestBlock = await this.indexer.getLatestBlock();
-    if(!latestBlock.success) {
-        this.inscriptionBalance.value = 0;
+    this.inscriptionBalance.value = 0;
+    if(!this.walletChainId || !this.walletAddress || !this.activeChain.value) {
         return;
     }
-    if(!this.walletAddress) {
-        this.inscriptionBalance.value = 0;
+    // TODO: the minted/{address} endpoint should take the chainId as a parameter
+    if(this.walletChainId !== this.activeChain.value.chainId) {
+        return;
+    }
+    const latestBlock = await this.indexer.getLatestBlock();
+    if(!latestBlock.success) {
         return;
     }
     const balanceForAccount = await this.indexer.getBalanceForAccount(this.walletAddress, latestBlock.data.latest_block);

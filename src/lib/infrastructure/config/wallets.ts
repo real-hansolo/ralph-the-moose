12f28cb784c4cf7f434dfdc5dc872f6df218bef1
maany <imptodefeat@gmail.com>
2024-03-23 02:21:08 +0100

    Wallets: remove non-erc wallets

--- src/lib/infrastructure/config/wallets.ts
+++ src/lib/infrastructure/config/wallets.ts
@@ -1,12 +1,9 @@
-import { bloctoWallet, coinbaseWallet, frameWallet, localWallet, metamaskWallet, phantomWallet, trustWallet, walletConnect } from "@thirdweb-dev/react";
+import { coinbaseWallet, localWallet, metamaskWallet, trustWallet, walletConnect } from "@thirdweb-dev/react";
 
 export const SUPPORTED_WALLETS = [
     metamaskWallet(),
-    walletConnect({ recommended: true }),
+    walletConnect(),
     coinbaseWallet(),
     localWallet(),
-    trustWallet({ recommended: true }),
-    bloctoWallet(),
-    frameWallet(),
-    phantomWallet(),
+    trustWallet(),
 ];

16fafdd0044f75098c8507b90e6d66bcca8d72d6
maany <imptodefeat@gmail.com>
2024-05-09 19:56:31 +0200

    thirdweb: remove old sdk ( v4)

--- src/lib/infrastructure/config/chains.tsx
+++ src/lib/infrastructure/config/chains.tsx
@@ -1,8 +1,7 @@
 import { env } from "~/env";
 import { IconNetworkBase } from "@maany_shr/ralph-the-moose-ui-kit";
 import { type Chain } from "thirdweb";
-import { base, baseSepolia } from "thirdweb/chains";
-import {  Base, BaseSepoliaTestnet } from "@thirdweb-dev/chains";
+import { base as Base, baseSepolia as BaseSepolia } from "thirdweb/chains";
 import { type TChainConfig as ChainConfig } from "@maany_shr/ralph-the-moose-ui-kit";
 
 export type TChainConfig = ChainConfig & {
@@ -10,12 +9,12 @@ export type TChainConfig = ChainConfig & {
 };
 
 export const BASE_MAINNET: TChainConfig = {
-  chainId: Base.chainId,
+  chainId: Base.id,
   name: "Base",
   rpcUrl: "https://mainnet.base.org",
   jsonRpcProvider: env.NEXT_PUBLIC_CHAIN_BASE_MAINNET_JSON_RPC,
-  explorerUrl: Base.explorers[0].url,
-  explorerName: Base.explorers[0].name,
+  explorerUrl: "https://base.basescan.org/",
+  explorerName: "BaseScan",
   gasLimit: 200000,
   icon: <IconNetworkBase />,
   networkCurrency: "ETH",
@@ -24,16 +23,16 @@ export const BASE_MAINNET: TChainConfig = {
   unwrappingFee: 0.00123,
   ralphReservoirAddress: "0x27e964E016B68EeFbe958Ace62174af0e1CeD053",
   ralphTokenAddress: "0x05F1938646a897008e813fb03CE7C575eAE45738",
-  thirdWeb: base,
+  thirdWeb: Base,
 };
 
 export const BASE_SEPOLIA_TESTNET: TChainConfig = {
-  chainId: BaseSepoliaTestnet.chainId,
+  chainId: BaseSepolia.id,
   name: "Base Sepolia",
   rpcUrl: env.NEXT_PUBLIC_CHAIN_BASE_SEPOLIA_JSON_RPC,
   jsonRpcProvider: env.NEXT_PUBLIC_CHAIN_BASE_SEPOLIA_JSON_RPC,
   explorerUrl: "https://sepolia.basescan.org/",
-  explorerName: BaseSepoliaTestnet.explorers[0].name,
+  explorerName: "BaseScan",
   gasLimit: 200000,
   icon: <IconNetworkBase />,
   networkCurrency: "ETH",
@@ -42,7 +41,7 @@ export const BASE_SEPOLIA_TESTNET: TChainConfig = {
   unwrappingFee: 0.00123,
   ralphReservoirAddress: "0x296271298780966bC0550a6B10c8e5BE60fE282f",
   ralphTokenAddress: "0xA415b2Df73D9E1D0415de58f012E2cB3E059F2E1",
-  thirdWeb: baseSepolia,
+  thirdWeb: BaseSepolia,
 };
 
 export const getSupportedChains = () => {

59302f1d3fc54fd3109dc3dc84dbba9344c12d12
maany <imptodefeat@gmail.com>
2024-05-12 19:01:13 +0200

    fix dto imports

--- src/lib/core/ports/secondary/wallet-provider-output-port.ts
+++ src/lib/core/ports/secondary/wallet-provider-output-port.ts
@@ -1,10 +1,14 @@
-import type { ActiveWalletDTO, ConnectedWalletsDTO, DisconnectWalletDTO, SupportedWalletsDTO } from "~/lib/infrastructure/dto/wallet-provider-dto";
-
+import type {
+  ActiveWalletDTO,
+  ConnectedWalletsDTO,
+  DisconnectWalletDTO,
+  SupportedWalletsDTO,
+} from "../../dto/wallet-provider-dto";
 
 export default interface WalletProviderOutputPort<TWalletInstance> {
-    getName(): string;
-    getConnectedWallets(): ConnectedWalletsDTO;
-    getActiveWallet(): ActiveWalletDTO<TWalletInstance>;
-    getSupportedWallets(): SupportedWalletsDTO<TWalletInstance>;
-    disconnect(wallet: TWalletInstance): DisconnectWalletDTO;
+  getName(): string;
+  getConnectedWallets(): ConnectedWalletsDTO;
+  getActiveWallet(): ActiveWalletDTO<TWalletInstance>;
+  getSupportedWallets(): SupportedWalletsDTO<TWalletInstance>;
+  disconnect(wallet: TWalletInstance): DisconnectWalletDTO;
 }

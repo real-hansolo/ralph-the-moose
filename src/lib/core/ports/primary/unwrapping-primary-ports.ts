d781df2c101888c3c28a0027c8895bf53ba54dce
maany <imptodefeat@gmail.com>
2024-05-15 21:44:09 +0200

    unwrapping: primary ports

---
+++ src/lib/core/ports/primary/unwrapping-primary-ports.ts
@@ -0,0 +1,16 @@
+import type { TUnwrappingErrorResponse, TUnwrappingProgressResponse, TUnwrappingRequest, TUnwrappingSuccessResponse, TUnwrappingTransactionGasStatus } from "../../usecase-models/unwrapping-usecase-models"
+
+export interface UnWrappingInputPort {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    presenter: UnWrappingOutputPort<any>
+    execute(request: TUnwrappingRequest): Promise<void>
+}
+
+export interface UnWrappingOutputPort<TResponse> {
+    response: TResponse
+    presentProgress(progress: TUnwrappingProgressResponse): void
+    presentSuccess(error: TUnwrappingSuccessResponse): void
+    presentError(response: TUnwrappingErrorResponse): void
+    presentEstimatedGas(response: TUnwrappingTransactionGasStatus): void
+}
+

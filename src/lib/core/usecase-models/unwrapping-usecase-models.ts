63e63fea1704b3cf46ccc5fcb8522211aeac5001
maany <imptodefeat@gmail.com>
2024-05-15 22:02:29 +0200

    unwrapping: presenter

--- src/lib/core/usecase-models/unwrapping-usecase-models.ts
+++ src/lib/core/usecase-models/unwrapping-usecase-models.ts
@@ -2,51 +2,43 @@ import { z } from "zod";
 import { ExecutedTransactionSchema, NetworkSchema, WalletSchema } from "../entity/models";
 
 export const UnwrappingRequestSchema = z.object({
-    network: NetworkSchema,
-    wallet: WalletSchema,
-    amount: z.number()
-})
+  network: NetworkSchema,
+  wallet: WalletSchema,
+  amount: z.number(),
+});
 
-export type TUnwrappingRequest = z.infer<typeof UnwrappingRequestSchema>
+export type TUnwrappingRequest = z.infer<typeof UnwrappingRequestSchema>;
 
 export const UnwrappingSuccessResponseSchema = z.object({
-    status: z.literal("success"),
-    transaction: ExecutedTransactionSchema,
-    message: z.string(),
-})
+  status: z.literal("success"),
+  transaction: ExecutedTransactionSchema,
+  message: z.string(),
+  amount: z.number(),
+});
 
-export type TUnwrappingSuccessResponse = z.infer<typeof UnwrappingSuccessResponseSchema>
+export type TUnwrappingSuccessResponse = z.infer<typeof UnwrappingSuccessResponseSchema>;
 
 export const UnwrappingErrorResponseSchema = z.object({
-    status: z.literal("error"),
-    message: z.string(),
-    details: z.object({
-        network: NetworkSchema,
-        wallet: WalletSchema,
-        amount: z.number(),
-        transaction: ExecutedTransactionSchema.optional(),
-        approvalError: z.boolean().default(false),
-    })
-})
-
-
-export type TUnwrappingErrorResponse = z.infer<typeof UnwrappingErrorResponseSchema>
-
-export const UnwrappingProgressResponseSchema = z.object({
-    transaction: ExecutedTransactionSchema.optional(),
-    amount: z.number(),
+  status: z.literal("error"),
+  message: z.string(),
+  details: z.object({
     network: NetworkSchema,
     wallet: WalletSchema,
-    estimatedGas: z.number().optional(),
-    message: z.string(),
-})
-
-export type TUnwrappingProgressResponse = z.infer<typeof UnwrappingProgressResponseSchema>
-
-export const UnwrappingTransactionGasStatusSchema = z.object({
-    status: z.literal("estimated-gas"),
-    estimatedGas: z.number(),
     amount: z.number(),
-})
+    transaction: ExecutedTransactionSchema.optional(),
+    approvalError: z.boolean().default(false),
+  }),
+});
 
-export type TUnwrappingTransactionGasStatus = z.infer<typeof UnwrappingTransactionGasStatusSchema>
+export type TUnwrappingErrorResponse = z.infer<typeof UnwrappingErrorResponseSchema>;
+
+export const UnwrappingProgressResponseSchema = z.object({
+  transaction: ExecutedTransactionSchema.optional(),
+  amount: z.number(),
+  network: NetworkSchema,
+  wallet: WalletSchema,
+  estimatedGas: z.number().optional(),
+  message: z.string(),
+});
+
+export type TUnwrappingProgressResponse = z.infer<typeof UnwrappingProgressResponseSchema>;

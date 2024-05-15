1f136e0558b683c3874101c6fe7cb759a90adfe0
maany <imptodefeat@gmail.com>
2024-05-15 15:12:15 +0200

    vitest errors

--- tests/client/vitest.config.ts
+++ tests/client/vitest.config.ts
@@ -1,15 +1,31 @@
 import { defineProject, mergeConfig } from "vitest/config";
 import sharedConfig from "../vitest.shared";
+import react from "@vitejs/plugin-react";
+import swc from "unplugin-swc";
+
 const config = mergeConfig(
   sharedConfig,
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   defineProject({
+    // optimizeDeps: {
+    //   esbuildOptions: {
+    //     tsconfigRaw: {
+    //       compilerOptions: {
+    //         experimentalDecorators: true,
+    //         emitDecoratorMetadata: true,
+    //       },
+    //     },
+    //   },
+    // },
     test: {
       name: "client",
       environment: "node",
       rootDir: "tests/client",
       include: ["**/*.test.ts"],
       globals: true,
+      allowJSX: true,
+      alias: {
+        // "react/jsx-runtime": "react/jsx-runtime.js",
+      },
       coverage: {
         provider: "v8",
         reporter: ["json", "json-summary", "html", "lcov", "text"],
@@ -21,15 +37,21 @@ const config = mergeConfig(
         //   functions: 10,
         //   lines: 10,
         // },
-        exclude: [
-          "tests/**/*.{ts,tsx}",
-          "**/*.config.{ts,tsx,js}",
-          ".storybook/",
-          "stories/**/*.{ts,tsx}",
-          ".eslint*",
-        ],
+        exclude: ["tests/**/*.{ts,tsx}", "**/*.config.{ts,tsx,js}", ".storybook/", "stories/**/*.{ts,tsx}", ".eslint*"],
       },
+      // plugins: [
+      //   react({
+      //     jsxRuntime: "classic",
+      //   }),
+       
+      // ],
     },
+    plugins: [
+     
+      swc.vite({
+        tsconfigFile: "./tsconfig.json",
+      })
+    ],
   }),
 );
 

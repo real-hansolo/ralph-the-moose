aefd297cfc1e2196c197299a2e0a64c325b1e675
maany <imptodefeat@gmail.com>
2024-05-15 21:39:43 +0200

    unwrap: usecase-models

---
+++ src/lib/core/usecase-models/unwrapping-usecase-models.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+import { ExecutedTransactionSchema, NetworkSchema, WalletSchema } from "../entity/models";
+
+export const UnwrappingRequestSchema = z.object({
+    network: NetworkSchema,
+    wallet: WalletSchema,
+    amount: z.number()
+})
+
+export type TUnwrappingRequest = z.infer<typeof UnwrappingRequestSchema>
+
+export const UnwrappingSuccessResponseSchema = z.object({
+    status: z.literal("success"),
+    transaction: ExecutedTransactionSchema,
+    message: z.string(),
+})
+
+export type TUnwrappingSuccessResponse = z.infer<typeof UnwrappingSuccessResponseSchema>
+
+export const UnwrappingErrorResponseSchema = z.object({
+    status: z.literal("error"),
+    message: z.string(),
+    details: z.object({
+        network: NetworkSchema,
+        wallet: WalletSchema,
+        amount: z.number(),
+        transaction: ExecutedTransactionSchema.optional(),
+        approvalError: z.boolean().default(false),
+    })
+})
+
+
+export type TUnwrappingErrorResponse = z.infer<typeof UnwrappingErrorResponseSchema>
+
+export const UnwrappingProgressResponseSchema = z.object({
+    transaction: ExecutedTransactionSchema.optional(),
+    amount: z.number(),
+    network: NetworkSchema,
+    wallet: WalletSchema,
+    estimatedGas: z.number().optional(),
+    message: z.string(),
+})
+
+export type TUnwrappingProgressResponse = z.infer<typeof UnwrappingProgressResponseSchema>

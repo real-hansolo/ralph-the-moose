fdb055ae6c627f50df89ed23160702128c84a631
maany <imptodefeat@gmail.com>
2024-05-15 20:14:18 +0200

    claiming: usecase-models

---
+++ src/lib/core/usecase-models/claiming-usecase-models.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+import { NetworkSchema, WalletSchema } from "../entity/models";
+
+export const ClaimingRequestSchema = z.object({
+    network: NetworkSchema,
+    wallet: WalletSchema,
+    amount: z.number()
+})
+
+export type TClaimingRequest = z.infer<typeof ClaimingRequestSchema>
+
+export const ClaimingSuccessResponseSchema = z.object({
+    status: z.literal("success"),
+    network: NetworkSchema,
+    wallet: WalletSchema,
+    amount: z.number(),
+    message: z.string()
+})
+
+export type TClaimingSuccessResponse = z.infer<typeof ClaimingSuccessResponseSchema>
+
+export const ClaimingErrorResponseSchema = z.object({
+    status: z.literal("error"),
+    message: z.string(),
+    details: z.object({
+        network: NetworkSchema,
+        wallet: WalletSchema,
+        amount: z.number(),
+    })
+})
+
+export type TClaimingErrorResponse = z.infer<typeof ClaimingErrorResponseSchema>
+
+export const ClaimingProgressResponseSchema = z.object({
+    amount: z.number(),
+    network: NetworkSchema,
+    wallet: WalletSchema,
+    message: z.string()
+})
+
+export type TClaimingProgressResponse = z.infer<typeof ClaimingProgressResponseSchema>

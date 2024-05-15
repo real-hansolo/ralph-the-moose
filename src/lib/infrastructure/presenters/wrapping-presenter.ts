7d49055a2a1d9508577ae1dbda3e362759c0ea31
maany <imptodefeat@gmail.com>
2024-05-15 17:45:31 +0200

    wrapping: presenter

---
+++ src/lib/infrastructure/presenters/wrapping-presenter.ts
@@ -0,0 +1,41 @@
+import type { TSignal } from "~/lib/core/entity/signals";
+import type { WrappingOutputPort } from "~/lib/core/ports/primary/wrapping-primary-ports";
+import type { TWrappingErrorResponse, TWrappingProgressResponse, TWrappingSuccessResponse } from "~/lib/core/usecase-models/wrapping-usecase-models";
+import type { TWrappingViewModel } from "~/lib/core/view-models/wrapping-view-model";
+
+export default class WrappingPresenter implements WrappingOutputPort<TSignal<TWrappingViewModel>> {
+  response: TSignal<TWrappingViewModel>;
+  constructor(response: TSignal<TWrappingViewModel>) {
+    this.response = response;
+  }
+
+  presentProgress(progress: TWrappingProgressResponse): void {
+    this.response.value.value = {
+      status: "in-progress",
+      message: progress.message,
+      amount: progress.amount,
+      wrapTransaction: progress.transaction,
+      wrapFound: progress.wrapFound,
+      s_gas_status: progress.s_gas_status,
+    };
+  }
+
+  presentError(error: TWrappingErrorResponse): void {
+    this.response.value.value = {
+      status: "error",
+      message: error.message,
+      amount: error.details.amount,
+      wrapTransaction: error.details.transaction,
+      wrapFound: error.details.wrapFound,
+    };
+  }
+
+  presentSuccess(success: TWrappingSuccessResponse): void {
+    this.response.value.value = {
+      status: "success",
+      message: "Wrapping successful",
+      amount: success.amount,
+      wrapTransaction: success.transaction,
+    };
+  }
+}

fae4e2ffc065858aa2b97634d4b4bfc77acd1d85
maany <imptodefeat@gmail.com>
2024-05-16 21:09:47 +0200

    bridging home

--- src/app/pages/RalphHome.tsx
+++ src/app/pages/RalphHome.tsx
@@ -4,26 +4,11 @@ import { RalphWalletCard } from "../_components/RalphWalletCard";
 import { RalphMintCard } from "../_components/RalphMintCard";
 import { RalphBalaceCard } from "../_components/RalphBalanceCard";
 import { useSignal } from "@preact/signals-react";
-import {
-  DEFAULT_CHAIN,
-  SUPPORTED_CHAINS,
-  type TChainConfig,
-} from "~/lib/infrastructure/config/chains";
-import {
-  useActiveAccount,
-  useActiveWallet,
-  useActiveWalletChain,
-} from "@maany_shr/thirdweb/react";
+import { DEFAULT_CHAIN, SUPPORTED_CHAINS, type TChainConfig } from "~/lib/infrastructure/config/chains";
+import { useActiveAccount, useActiveWallet, useActiveWalletChain } from "@maany_shr/thirdweb/react";
 import { type Chain } from "@maany_shr/thirdweb";
-import {
-  PageTemplate,
-  type ToastProps,
-} from "@maany_shr/ralph-the-moose-ui-kit";
-import { GATEWAYS } from "~/lib/infrastructure/config/ioc/symbols";
-import type NetworkGatewayOutputPort from "~/lib/core/ports/secondary/network-gateway-output-port";
-import { type TNetwork } from "~/lib/core/entity/models";
-import type IndexerGatewayOutputPort from "~/lib/core/ports/secondary/indexer-gateway-output-port";
-import { clientContainer } from "~/lib/infrastructure/config/ioc/container";
+import { Button, LightFrame, PageTemplate, type ToastProps } from "@maany_shr/ralph-the-moose-ui-kit";
+import { bridge } from "../_components/controllers/BridgeController";
 
 export const RalphHome = () => {
   /**
@@ -34,21 +19,6 @@ export const RalphHome = () => {
   const connectedWalletNetwork: Chain | undefined = useActiveWalletChain();
   const isWalletConnected = connectedWallet !== undefined;
 
-  const indexerFactory: (network: TNetwork) => IndexerGatewayOutputPort = clientContainer.get(GATEWAYS.INDEXER_GATEWAY_FACTORY);
-  const networkGateway: NetworkGatewayOutputPort = clientContainer.get<NetworkGatewayOutputPort>(GATEWAYS.NETWORK_GATEWAY);
-  const activeNetworkDTO = networkGateway.getActiveNetwork();
-  if (!activeNetworkDTO.success) {
-    throw new Error("Error getting active network");
-  }
-  const tempActiveNetwork = activeNetworkDTO.data;
-  const indexer = indexerFactory(tempActiveNetwork);
-  indexer.getAllMinted()
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((e) => {
-      console.log("Error fetching minted", e);
-    });
   
   /**
    * [Signal] Toasts: Store the toasts to be displayed on the screen.
@@ -58,14 +28,17 @@ export const RalphHome = () => {
 
   return (
     <div id="app-container">
-      <PageTemplate
-        toasts={toasts}
-        supportedNetworks={SUPPORTED_CHAINS}
-        activeNetwork={activeNetwork}
-      >
-        {!isWalletConnected && (
-          <RalphWalletCard/>
-        )}
+      <PageTemplate toasts={toasts} supportedNetworks={SUPPORTED_CHAINS} activeNetwork={activeNetwork}>
+        {!isWalletConnected && <RalphWalletCard />}
+        <LightFrame>
+          <Button variant="primary" label="Bridge" onClick={() => {
+            bridge(connectedWallet).then(() => {
+              console.log("Bridge Success");
+            }).catch((e) => {
+              console.log("Bridge Error", e);
+            });
+          }}/>
+        </LightFrame>
 
         <RalphMintCard
           toasts={toasts}
@@ -83,9 +56,7 @@ export const RalphHome = () => {
             connectedWalletNetwork={connectedWalletNetwork}
           />
         }
-        {isWalletConnected && (
-          <RalphWalletCard/>
-        )}
+        {isWalletConnected && <RalphWalletCard />}
       </PageTemplate>
     </div>
   );

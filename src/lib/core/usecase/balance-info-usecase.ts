54c19c39e3fcd3cdb220fbee92caf606515cafb2
real-hansolo <real-hansolo@elklabs.org>
2024-12-10 22:30:25 +0100

    enable balance info

--- src/lib/core/usecase/balance-info-usecase.ts
+++ src/lib/core/usecase/balance-info-usecase.ts
@@ -41,35 +41,35 @@ export default class BalanceInfoUsecase implements BalanceInfoInputPort {
     }
     const wrappedBalance = wrappedBalanceDTO.data.balance;
 
-    // const inscriptionBalanceDTO = await indexerGateway.getBalanceForAccount(wallet.activeAccount);
-    // if (!inscriptionBalanceDTO.success) {
-    //   this.presenter.presentError({
-    //     status: "error",
-    //     message: `Error getting inscription balance on ${network.name} for wallet ${wallet.activeAccount}. If you're like a nerd or sth, here's the message from the backend: ${inscriptionBalanceDTO.data.msg}`,
-    //     details: {
-    //       network: network,
-    //       wallet: wallet,
-    //     },
-    //   });
-    //   return;
-    // }
+    const inscriptionBalanceDTO = await indexerGateway.getBalanceForAccount(wallet.activeAccount);
+    if (!inscriptionBalanceDTO.success) {
+      this.presenter.presentError({
+        status: "error",
+        message: `Error getting inscription balance on ${network.name} for wallet ${wallet.activeAccount}. If you're like a nerd or sth, here's the message from the backend: ${inscriptionBalanceDTO.data.msg}`,
+        details: {
+          network: network,
+          wallet: wallet,
+        },
+      });
+      return;
+    }
 
-    const inscriptionBalance = 0 //inscriptionBalanceDTO.data.balance;
+    const inscriptionBalance = inscriptionBalanceDTO.data.balance;
 
-    // const claimableAmountDTO = await this.ralphReservoirGateway.getClaimableAmount(wallet.activeAccount, network);
-    // if (!claimableAmountDTO.success) {
-    //   this.presenter.presentError({
-    //     status: "error",
-    //     message: `Error getting claimable amount on ${network.name} for wallet ${wallet.activeAccount}. If you're like a nerd or sth, here's the message from the backend: ${claimableAmountDTO.data.message}`,
-    //     details: {
-    //       network: network,
-    //       wallet: wallet,
-    //     },
-    //   });
-    //   return;
-    // }
+    const claimableAmountDTO = await this.ralphReservoirGateway.getClaimableAmount(wallet.activeAccount, network);
+    if (!claimableAmountDTO.success) {
+      this.presenter.presentError({
+        status: "error",
+        message: `Error getting claimable amount on ${network.name} for wallet ${wallet.activeAccount}. If you're like a nerd or sth, here's the message from the backend: ${claimableAmountDTO.data.message}`,
+        details: {
+          network: network,
+          wallet: wallet,
+        },
+      });
+      return;
+    }
 
-    const claimableAmount = 0 //claimableAmountDTO.data.amount;
+    const claimableAmount = claimableAmountDTO.data.amount;
 
     this.presenter.presentSuccess({
       status: "success",
